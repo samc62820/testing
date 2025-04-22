17a33873e2393b7c9637dcbd32f8cf098c75bb91
Samreen <samc62820@gmail.com>
2025-04-22 10:23:48 +0530

    joi middleware

--- src/helpers/userValidation.js
+++ src/helpers/userValidation.js
@@ -1,6 +1,6 @@
 const Joi =  require('joi');
 
-const schema = Joi.object({
+const createUserSchema = Joi.object({
     userName: Joi.string().alphanum().min(3).max(30).required(),
    profile: {
     firstName : Joi.string().alphanum().min(3).max(30).required(),
@@ -9,4 +9,36 @@ const schema = Joi.object({
    }
 });
 
-module.exports = schema;
+  // middlewares/validateUser.js
+  module.exports = function validateUserSchema(req, res, next) {
+    const { error } = createUserSchema.validate(req.body);
+    if (error) {
+      return res.status(400).json({ message: error.details[0].message });
+    }
+    next();
+  };
+
+const updateUserSchema = Joi.object({
+    userName: Joi.string().alphanum().min(3).max(30),
+   profile: {
+    firstName : Joi.string().alphanum().min(3).max(30),
+    lastName : Joi.string().alphanum().min(3).max(30),
+    age: Joi.number().integer().min(1900).max(2013),
+   }
+});
+// Middleware
+module.exports = function validateUpdateUser(req, res, next) {
+    const { error } = updateUserSchema.validate(req.body, { abortEarly: false });
+  
+    if (error) {
+      return res.status(400).json({
+        status: 'error',
+        message: 'Validation failed',
+        details: error.details.map(detail => detail.message),
+      });
+    }
+  
+    next();
+  }
+
+
